efe41bf4feccd06b3f1c0e5a09fee961bcda46f8
Krishika Shivnani <krishika510@gmail.com>
2021-11-07 23:00:52 -0500

    Project

---
+++ blog-frontend/src/App.js
@@ -0,0 +1,48 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+
+
+// 	PostList() {
+// 		$.getJSON('')
+// 	}
+
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import { Router } from "@reach/router";
+
+import Posts from './components/posts'
+import Post from './components/post'
+
+function App() {
+  return (
+    <Router>
+      <Posts path="/" />
+      <Post path="/posts/:id" />
+    </Router>
+  );
+}
+
+export default App;
